87dc76f91669704e197d9ff5696c67181d6e1e69
NATBLAU <92014159+NATBLAU@users.noreply.github.com>
2022-10-28 13:10:15 +0200

    smth

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {GoogleMap, MarkerF} from "@react-google-maps/api"
+
+
+
+function App() {
+  return (
+   
+    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=aarhus&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+   
+   
+  );
+}
+
+export default App;
